7ec6b01d43036e047394fabdcc01b18fe85b9919
TheFuzzy <Infestor51@gmail.com>
2014-03-09 21:49:16 +0800

    Added view logic and url endpoints to socialnetwork

--- backend/storm/socialnetwork/urls.py
+++ backend/storm/socialnetwork/urls.py
@@ -3,4 +3,9 @@ from socialnetwork import views
 
 urlpatterns = patterns(
     '',
+    url(r'^$', views.social_login, name="socialnetwork.social_login"),
+    url(r'^post$', views.social_post, name="socialnetwork.social_post"),
+    url(r'^(?P<site>\w+)/logout$', views.social_logout, name="socialnetwork.social_logout"),
+    url(r'^facebook$', views.facebook_page_select, name="socialnetwork.facebook_page_select"),
+    url(r'^facebook_process$', views.facebook_process, name="socialnetwork.facebook_process"),
 )
