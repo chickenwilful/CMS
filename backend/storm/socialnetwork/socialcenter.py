58b6a86699732a6e3267ff92b9bb65458e9c9766
TheFuzzy <Infestor51@gmail.com>
2014-03-12 15:26:17 +0800

    Fixed missing import in SocialCenter causing it to crash.

--- backend/storm/socialnetwork/socialcenter.py
+++ backend/storm/socialnetwork/socialcenter.py
@@ -3,7 +3,7 @@ from models import SocialToken
 from datetime import datetime
 import logging
 
-from socialbot import FacebookBot, TwitterBot, GPlusBot
+from socialbot import SocialBot, FacebookBot, TwitterBot, GPlusBot
 
 logger = logging.getLogger('storm')
 
