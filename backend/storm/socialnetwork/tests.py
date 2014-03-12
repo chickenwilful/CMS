fe830e691c2dac0ee38705a21de3121a576df6ca
TheFuzzy <Infestor51@gmail.com>
2014-03-12 15:45:37 +0800

    Added URL resolution tests for all views in socialnetwork

--- backend/storm/socialnetwork/tests.py
+++ backend/storm/socialnetwork/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.core.urlresolvers import resolve
 import random
 import string
 import urllib
 
+import socialnetwork.views as sn_views
 from socialnetwork.socialbot import SocialBot
 from socialnetwork.socialcenter import SocialCenter
 
@@ -97,3 +100,49 @@ class SocialCenterTest(TestCase):
         
         self.assertEqual(redirect_url, "http://test.com/auth?redirect_uri=%s" % urllib.quote_plus(callback_url))
         self.assertEqual(auth_data["state"], "test_state")
+
+class URLTest(TestCase):
+    
+    def test_social_url_resolves_to_social_view(self):
+        found = resolve("/social/")
+        self.assertEqual(found.func, sn_views.social)
+    
+    def test_social_test_url_resolves_to_social_test_view(self):
+        found = resolve("/social/test")
+        self.assertEqual(found.func, sn_views.social_test)
+    
+    def test_social_post_url_resolves_to_social_post_view(self):
+        found = resolve("/social/post")
+        self.assertEqual(found.func, sn_views.social_post)
+    
+    def test_logout_url_resolves_to_logout_view(self):
+        #Ensure multiple alphanumeric URLs reach the same function
+        for _ in range(10):
+            site_name = generate_random_string(10, string.ascii_letters + string.digits)
+            found = resolve("/social/%s/logout" % site_name)
+            self.assertEqual(found.func, sn_views.social_logout)
+    
+    def test_facebook_page_url_resolves_to_facebook_page_view(self):
+        found = resolve("/social/facebook/page")
+        self.assertEqual(found.func, sn_views.facebook_page_select)
+    
+    def test_facebook_process_url_resolves_to_facebook_process_view(self):
+        found = resolve("/social/facebook/process")
+        self.assertEqual(found.func, sn_views.facebook_process)
+    
+    def test_twitter_auth_url_resolves_to_twitter_auth_view(self):
+        found = resolve("/social/twitter/auth")
+        self.assertEqual(found.func, sn_views.twitter_auth)
+    
+    def test_twitter_callback_url_resolves_to_twitter_callback_view(self):
+        found = resolve("/social/twitter/callback")
+        self.assertEqual(found.func, sn_views.twitter_callback)
+    
+    def test_gplus_auth_url_resolves_to_gplus_auth_view(self):
+        found = resolve("/social/gplus/auth")
+        self.assertEqual(found.func, sn_views.gplus_auth)
+    
+    def test_gplus_callback_url_resolves_to_gplus_callback_view(self):
+        found = resolve("/social/gplus/callback")
+        self.assertEqual(found.func, sn_views.gplus_callback)
+
