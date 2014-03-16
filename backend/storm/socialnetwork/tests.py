b1b02615c8b946b5e4ef76f31b593976351b061e
TheFuzzy <Infestor51@gmail.com>
2014-03-16 17:15:45 +0800

    Updated socialnetwork unit tests to reflect new logic.

--- backend/storm/socialnetwork/tests.py
+++ backend/storm/socialnetwork/tests.py
@@ -9,14 +9,39 @@ import socialnetwork.views as sn_views
 from socialnetwork.socialbot import SocialBot
 from socialnetwork.socialcenter import SocialCenter
 
-def generate_random_string(size, chars):
-    return ''.join(random.choice(chars) for _ in range(size))
-
 class TestBot(SocialBot):
-
+    """Implementation of SocialBot for tests
+    """
+    __client_token_name = "test_code"
+    
+    # Exposed fields, so that tests can use them for comparisons
+    _session_token = "fjsd0hnf02HNR)"
+    
+    def must_refresh_token(self):
+        return False
+    
+    def must_select_page(self):
+        return False
+    
+    def get_site_name(self):
+        return "Test Site"
+    
+    def get_client_token_name(self):
+        return __client_token_name
+    
+    def get_account_name(self):
+        if hasattr(self, "main_token"):
+            return "Test Site Account"
+        return "Unauthorized"
+    
+    def get_account_url(self):
+        if hasattr(self, "_main_token"):
+            return "https://test.com/page"
+        return "Unauthorized"
+    
     def post(self, title, content, link):
         result = {}
-        if not any(title, content):
+        if not (title or content):
             result["error"] = "No content in post."
         else:
             if title:
@@ -41,25 +66,31 @@ class TestBot(SocialBot):
         pass
     
     def start_authentication(self, callback_url):
-        test_auth_data = {"state": "test_state"}
-        return "http://test.com/auth?redirect_uri=%s" % urllib.quote_plus(callback_url), test_auth_data
+        auth_data = {}
+        
+        auth_data["session_token"] = self._session_token
+        auth_data["callback_url"] = callback_url
+        
+        return "http://test.com/auth?redirect_uri=%s" % urllib.quote_plus(callback_url), auth_data
     
-    def process_token(self, client_token, **kwargs):
+    def process_token(self, client_token, auth_data):
         result = {}
         
-        if client_token != "valid_code":
+        # These assignments will crash tests if the keys don't exist
+        session_token = auth_data["session_token"]
+        callback_url = auth_data["callback_url"]
+        
+        if session_token != self._session_token:
+            result["error"] = "Invalid session token"
+        elif client_token != "valid_code":
             result["error"] = "Invalid code"
         else:
             result["main_token"] = "test_main_token"
         
-        pass
+        return result
     
     def get_pages(self, request_token):
-        pages = []
-        if request_token == "valid_request_token":
-            for num in range(10):
-                pages.append({ id: "%d" % num, name: "Page %d" % num })
-        return pages
+        raise NotImplementedError("This SocialBot has no pages.")
     
     def clear_token(self):
         if hasattr(self, "main_token"):
@@ -67,28 +98,58 @@ class TestBot(SocialBot):
         if hasattr(self, "sub_token"):
             del self.sub_token
 
+class TestBotWithPages(TestBot):
+    """Implementation of SocialBot for tests
+    """
+    
+    def must_select_page(self):
+        return True
+    
+    def get_site_name(self):
+        return "Test Paged Site"
+    
+    def get_pages(self, request_token):
+        pages = []
+        if request_token == "valid_request_token":
+            for num in range(10):
+                pages.append({ "id" : "%d" % (num+1), "name" : "Page %d" % (num+1) })
+        return pages
+
 class SocialCenterTest(TestCase):
 
     TEST_SITE = "test"
+    TEST_PAGED_SITE = "test_paged"
     
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.test_bot = TestBot()
+        self.test_paged_bot = TestBotWithPages()
         self.social_center = SocialCenter()
         self.social_center.add_site(self.TEST_SITE, self.test_bot)
+        self.social_center.add_site(self.TEST_PAGED_SITE, self.test_paged_bot)
+    
+    @classmethod
+    def tearDownClass(self):
+        self.social_center.logout(self.TEST_SITE)
+        self.social_center.logout(self.TEST_PAGED_SITE)
+        del self.social_center
+        del self.test_bot
+        del self.test_paged_bot
     
     def test_social_center_is_singleton(self):
         social_center = SocialCenter()
-        self.assertTrue(self.social_center is social_center)
+        self.assertIs(self.social_center, social_center)
     
     def test_social_center_only_accepts_social_bots(self):
         self.assertRaises(ValueError, self.social_center.add_site, "invalid site", 434)
         self.assertRaises(ValueError, self.social_center.add_site, 1342, TestBot())
     
-    def test_social_bot_is_logged_out(self):
+    def test_social_bot_is_initially_logged_out(self):
         self.assertFalse(self.social_center.is_logged_in(self.TEST_SITE))
     
     def test_social_bot_can_authenticate(self):
         self.social_center.authenticate(self.TEST_SITE, "test_main_token", "test_sub_token")
+        
         self.assertEqual(self.test_bot.main_token, "test_main_token")
         self.assertEqual(self.test_bot.sub_token, "test_sub_token")
         self.assertTrue(self.social_center.is_logged_in(self.TEST_SITE))
@@ -99,9 +160,107 @@ class SocialCenterTest(TestCase):
         redirect_url, auth_data = self.social_center.start_authentication(self.TEST_SITE, callback_url)
         
         self.assertEqual(redirect_url, "http://test.com/auth?redirect_uri=%s" % urllib.quote_plus(callback_url))
-        self.assertEqual(auth_data["state"], "test_state")
+        self.assertEqual(auth_data["session_token"], self.test_bot._session_token)
+        self.assertEqual(auth_data["callback_url"], callback_url)
+        
+    def test_social_bot_can_process_client_token(self):
+        callback_url = "http://testapp2.com/"
+        redirect_url, auth_data = self.social_center.start_authentication(self.TEST_SITE, callback_url)
+        
+        self.assertRaises(BaseException, self.social_center.process_client_token, None)
+        self.assertRaises(BaseException, self.social_center.process_client_token, {})
+        self.assertRaises(BaseException, self.social_center.process_client_token, { "callback_url" : callback_url })
+        
+        invalid_result = self.social_center.process_client_token(self.TEST_SITE, "invalid_code", auth_data)
+        
+        self.assertNotIn("main_token", invalid_result)
+        self.assertIn("error", invalid_result)
+        
+        modified_data = auth_data.copy()
+        modified_data["session_token"] = "invalidSessionToken"
+        modified_result = self.social_center.process_client_token(self.TEST_SITE, "valid_code", modified_data)
+        
+        self.assertNotIn("main_token", modified_result)
+        self.assertIn("error", modified_result)
+        
+        valid_result = self.social_center.process_client_token(self.TEST_SITE, "valid_code", auth_data)
+        
+        self.assertIn("main_token", valid_result)
+        self.assertNotIn("error", valid_result)
+    
+    def test_only_paged_bot_has_pages(self):
+        self.assertFalse(self.social_center.must_select_page(self.TEST_SITE))
+        self.assertTrue(self.social_center.must_select_page(self.TEST_PAGED_SITE))
+        
+        self.assertRaises(NotImplementedError, self.social_center.get_pages, self.TEST_SITE, "any_token")
+        # Should not raise any errors:
+        pages = self.social_center.get_pages(self.TEST_PAGED_SITE, "valid_request_token")
+    
+    def test_social_bot_can_get_pages(self):
+        self.social_center.authenticate(self.TEST_PAGED_SITE, "test_main_token", "test_sub_token")
+        
+        invalid_pages = self.social_center.get_pages(self.TEST_PAGED_SITE, "invalid_request_token")
+        self.assertFalse(invalid_pages)
+        
+        valid_pages = self.social_center.get_pages(self.TEST_PAGED_SITE, "valid_request_token")
+        self.assertTrue(valid_pages)
+        self.assertEqual(len(valid_pages), 10)
+        for i in range(10):
+            page = valid_pages[i]
+            self.assertEqual(page["id"], str(i+1))
+            self.assertEqual(page["name"], "Page " + str(i+1))
+    
+    def test_social_bot_can_logout(self):
+        self.social_center.logout(self.TEST_SITE)
+        
+        self.assertFalse(self.social_center.is_logged_in(self.TEST_SITE))
+        self.assertFalse(hasattr(self.test_bot, "main_token"))
+        self.assertFalse(hasattr(self.test_bot, "sub_token"))
+    
+    def test_social_bot_can_only_post_while_logged_in(self):
+        title = "Test title"
+        content = "Test content"
+        link = "Test link"
+    
+        self.assertFalse(self.social_center.is_logged_in(self.TEST_SITE))
+        
+        logged_out_valid_results = self.social_center.publish(title, content, link)
+        logged_out_invalid_results = self.social_center.publish(None, None, None)
+        self.assertFalse(logged_out_valid_results)
+        self.assertFalse(logged_out_invalid_results)
+        
+        self.social_center.authenticate(self.TEST_SITE, "main_token", "sub_token")
+        #import pdb; pdb.set_trace()
+        logged_in_valid_results = self.social_center.publish(title, content, link)
+        logged_in_invalid_results = self.social_center.publish(None, None, None)
+        self.assertTrue(logged_in_valid_results)
+        self.assertTrue(logged_in_invalid_results)
+        
+        valid_result = logged_in_valid_results[0]
+        self.assertEqual(valid_result["site"], self.TEST_SITE)
+        self.assertNotIn("error", valid_result)
+        self.assertIn("result", valid_result)
+        self.assertEqual(valid_result["result"]["title"], title)
+        self.assertEqual(valid_result["result"]["content"], content)
+        self.assertEqual(valid_result["result"]["link"], link)
+        
+        invalid_result = logged_in_invalid_results[0]
+        self.assertEqual(invalid_result["site"], self.TEST_SITE)
+        self.assertIn("result", invalid_result)
+        self.assertIn("error", invalid_result["result"])
 
 class URLTest(TestCase):
+    
+    @staticmethod
+    def generate_random_string(size, chars):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    # Ensure multiple alphanumeric URLs reach the same function
+    def assert_dynamic_url_resolves_to(self, dynamic_url, view_func):
+        for _ in range(10):
+            site_name = self.generate_random_string(10, string.ascii_letters + string.digits)
+            found = resolve(dynamic_url % site_name)
+            self.assertEqual(found.func, view_func)
     
     def test_social_url_resolves_to_social_view(self):
         found = resolve("/social/")
@@ -116,33 +275,23 @@ class URLTest(TestCase):
         self.assertEqual(found.func, sn_views.social_post)
     
     def test_logout_url_resolves_to_logout_view(self):
-        #Ensure multiple alphanumeric URLs reach the same function
-        for _ in range(10):
-            site_name = generate_random_string(10, string.ascii_letters + string.digits)
-            found = resolve("/social/%s/logout" % site_name)
-            self.assertEqual(found.func, sn_views.social_logout)
+        self.assert_dynamic_url_resolves_to("/social/%s/logout", sn_views.social_logout)
     
-    def test_facebook_page_url_resolves_to_facebook_page_view(self):
-        found = resolve("/social/facebook/page")
-        self.assertEqual(found.func, sn_views.facebook_page_select)
+    def test_auth_url_resolves_to_auth_view(self):
+        self.assert_dynamic_url_resolves_to("/social/%s/auth", sn_views.social_auth)
     
-    def test_facebook_process_url_resolves_to_facebook_process_view(self):
-        found = resolve("/social/facebook/process")
-        self.assertEqual(found.func, sn_views.facebook_process)
+    def test_callback_url_resolves_to_callback_view(self):
+        self.assert_dynamic_url_resolves_to("/social/%s/callback", sn_views.social_callback)
     
-    def test_twitter_auth_url_resolves_to_twitter_auth_view(self):
-        found = resolve("/social/twitter/auth")
-        self.assertEqual(found.func, sn_views.twitter_auth)
-    
-    def test_twitter_callback_url_resolves_to_twitter_callback_view(self):
-        found = resolve("/social/twitter/callback")
-        self.assertEqual(found.func, sn_views.twitter_callback)
-    
-    def test_gplus_auth_url_resolves_to_gplus_auth_view(self):
-        found = resolve("/social/gplus/auth")
-        self.assertEqual(found.func, sn_views.gplus_auth)
-    
-    def test_gplus_callback_url_resolves_to_gplus_callback_view(self):
-        found = resolve("/social/gplus/callback")
-        self.assertEqual(found.func, sn_views.gplus_callback)
+    def test_page_select_url_resolves_to_page_select_view(self):
+        self.assert_dynamic_url_resolves_to("/social/%s/page", sn_views.social_page_select)
 
+class LogicTest(TestCase):
+    
+    def test_social_view_has_correct_elements(self):
+        request = HttpRequest()
+        request.method = "GET"
+        response = sn_views.social(request)
+        #render_to_string()
+        self.assertTrue(True)
+        
